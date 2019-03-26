a25e3852d495ddc032c325536e8815c4c2c0d2b2
hjrugk <tassjjjj@live.cn>
2019-03-26 17:34:00 +0800

    公演录播需要跨域的太多了

--- app.js
+++ app.js
@@ -14,7 +14,15 @@ app.use(bodyParser.urlencoded({
 // parse application/json
 app.use(bodyParser.json())
 
-app.use(router);
+app.use(router)
+
+app.all('*', function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Methods', '*');
+  res.header('Content-Type', 'application/json;charset=utf-8');
+  next();
+})
 
 app.use('/',express.static(path.join(__dirname, 'dist'),{index: 'index.html'}))
 
